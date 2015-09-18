139be2cd5e2aad874d07f5dfcaecf080cde6ef54
admin <wd4est@gmail.com>
2015-09-18 15:13:04 -0400

    Initial Commit:

---
+++ SocketInt.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-09-18T14:16:14
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = SocketInt
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_ClientButton_clicked()
+{
+
+}
+
+void MainWindow::on_HostButton_clicked()
+{
+
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    qApp->quit();
+}
